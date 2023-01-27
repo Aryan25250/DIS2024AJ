c1a147855a511f16288cbe8dc6f0254b23c11a3c
Aryan25250 <123520080+Aryan25250@users.noreply.github.com>
2023-01-27 12:18:03 +1000

    Create mypython01.py

---
+++ Unit1_Create_With_Code/mypython01.py
@@ -0,0 +1,15 @@
+# This is a comment
+"""
+This is a multi
+line comment
+"""
+
+# Input - asking for a user to input something and setting the result as a variable
+name = input("What is your name? ")
+age = input("What is your age? ")
+
+# Processing
+# Python can calculate numbers
+age = 20 + age
+
+print("Hi, "+name+", Your age in 20 years is "+age)
