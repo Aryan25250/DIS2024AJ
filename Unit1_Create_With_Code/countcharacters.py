6f1c8ce0d66901dbc217d77f159ac58c30881cf1
Aryan <25250@stpauls.qld.edu.au>
2023-01-31 14:32:13 +1000

    test

--- Unit1_Create_With_Code/countcharacters.py
+++ Unit1_Create_With_Code/countcharacters.py
@@ -1,1 +1,8 @@
+while True:
+    string = input("Enter the title of the book to count characters for: ")
+    numChars = str(len(string))
 
+    response = input("The length is "+numChars+" characters long. Type 'yes' to run the program again: ")
+
+    if response != "yes":
+        break
