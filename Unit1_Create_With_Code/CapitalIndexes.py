5205a39462275b0d02097e84b6067532c44178f2
Aryan <25250@stpauls.qld.edu.au>
2023-02-02 16:52:35 +1000

    test

---
+++ Unit1_Create_With_Code/CapitalIndexes.py
@@ -0,0 +1,18 @@
+print("=== Capital Indexes ===\n")
+
+def capitalIndexes(string):
+    indexes = []
+    for i, v in enumerate(string):
+        if v.upper() == v:
+            indexes.append(i)
+
+    return indexes
+
+while True:
+    string = input("Enter string or 'q' to quit: ")
+
+    if string.lower() == "q":
+        break
+
+    indexes = capitalIndexes(string)
+    print(indexes)
