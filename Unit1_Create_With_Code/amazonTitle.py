bec80284084d21c4e269e525da1e01143c83e521
Aryan <25250@stpauls.qld.edu.au>
2023-02-02 16:40:56 +1000

    test

--- Unit1_Create_With_Code/amazonTitle.py
+++ Unit1_Create_With_Code/amazonTitle.py
@@ -11,6 +11,6 @@ while True:
     response = "Word count: "+str(numWords)
     print(response)
 
-    if numWords > 2:
+    if numWords > 5:
         print("\n⚠️ WARNING ⚠️")
-        print("Product titles longer than 2 words don't sell as well. It is recommended to shorten your title to 2 words or less.")
+        print("Product titles longer than 5 words don't sell as well. It is recommended to shorten your title for optimal sales.")
