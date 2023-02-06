806c1b98ee5711adaa6ecdecbb44c4a5535fa7a4
Aryan <25250@stpauls.qld.edu.au>
2023-02-06 12:45:17 +1000

    test

--- Unit1_Create_With_Code/numberGuessingGame.py
+++ Unit1_Create_With_Code/numberGuessingGame.py
@@ -5,19 +5,45 @@ numGuesses = 10
 range1 = 1
 range2 = 100
 
-def game():
-    print("\nWelcome to the number guessing game!")
-    time.sleep(.5)
-    input("Respond with anything to start!: ")
+levels = {
+    "Easy": {
+        "numGuesses": 15,
+        "range1": 1,
+        "range2": 100,
+    },
+    "Medium": {
+        "numGuesses": 10,
+        "range1": 1,
+        "range2": 250,
+    },
+    "Hard": {
+        "numGuesses": 7,
+        "range1": 1,
+        "range2": 500,
+    }
+}
+
+levelsBeat = []
+
+#ask for users name, respond hello name welcome
+#at the end, tell how many guesses it took
+#3 different levels (easy, medium, hard)
+#high score table
+
+def game(level,settings):
+    thisRange1 = settings["range1"]
+    thisRange2 = settings["range2"]
+    thisNumGuesses = settings["numGuesses"]
 
     print("Generating number...")
     time.sleep(2)
-    number = random.randint(range1,range2)
+    number = random.randint(thisRange1,thisRange2)
 
-    for i in range(numGuesses):
-        print("\n--GUESS "+str(i+1)+"/"+str(numGuesses)+"--\n")
+    won = False
+    for i in range(thisNumGuesses):
+        print("\n--GUESS "+str(i+1)+"/"+str(thisNumGuesses)+"--\n")
 
-        guess = int(input("Guess a number between "+str(range1)+" and "+str(range2)+": "))
+        guess = int(input("Guess a number between "+str(thisRange1)+" and "+str(thisRange2)+": "))
 
         time.sleep(.2)
 
@@ -26,15 +52,65 @@ def game():
         elif guess > number:
             print("Too high!")
         elif guess == number:
-            print("Congrats! You won!")
+            won = True
+
+            if not level in levelsBeat:
+                levelsBeat.append(level)
+
+            print("Congrats! You beat "+level+" mode! It took you "+str(i+1)+" guesses!")
             time.sleep(.5)
             break
 
         time.sleep(.2)
 
+    if not won:
+        print("You lost! The number was: "+str(number))
+
     response = input("Respond with 'yes' to play again: ")
     if response.lower() != "yes":
         quit()
 
+def introduction():
+    name = input("What's your name?: ")
+
+    print("Hello, "+name+"! Welcome to the number guessing game!")
+
+    time.sleep(1)
+
+def getLevel():
+    string = "\nWhich level would you like to play?\n\n"
+
+    for i, (k,v) in enumerate(levels.items()):
+        emoji = "❌"
+        if k in levelsBeat:
+            emoji = "✅"
+
+        string += "("+str(i+1)+") "
+        string += k + " "+emoji+"\n"
+
+    chosenLevel = None
+    while not chosenLevel:
+        thisChosenLevel = input(string)
+
+        if thisChosenLevel.capitalize() in levels:
+            chosenLevel = thisChosenLevel.capitalize()
+
+    return chosenLevel
+
+def getAction():
+    chosenAction = None
+
+    while not chosenAction:
+        thisAction = input("(1) Play\n(2) Scoreboard\n(3) Quit\n")
+
+        if thisAction == "1" or thisAction.lower == "play":
+            chosenAction = "Play"
+
+introduction()
+
 while True:
-    game()
+    action = getAction()
+
+    level = getLevel()
+
+    game(level,levels[level])
