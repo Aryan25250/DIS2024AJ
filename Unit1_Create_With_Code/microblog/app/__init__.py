ab7bd03a0e195ba065a0a84146bf42b0c0895f8f
Aryan <25250@stpauls.qld.edu.au>
2023-03-06 12:45:17 +1000

    test

--- Unit1_Create_With_Code/microblog/app/__init__.py
+++ Unit1_Create_With_Code/microblog/app/__init__.py
@@ -1,7 +1,17 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+print("test")
+print(Config.SQLALCHEMY_DATABASE_URI)
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.config["SECRET_KEY"] = "long_secret_key"
 
-from app import routes
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
+
+
+from app import routes, models
